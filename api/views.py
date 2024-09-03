8098386b238bc59be6b3820adf2e7161bc8a35af
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-09-03 16:47:00 -0500

    Changes again

--- api/views.py
+++ api/views.py
@@ -34,7 +34,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .serializers import RegisterSerializer
-import serializers
+from rest_framework import serializers
 
 logger = logging.getLogger(__name__)
 
