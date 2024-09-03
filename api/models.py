a81cbc67ced17d4b61849601798908fca4e51d9b
Zachary Cherney <zach@Zacharys-MacBook-Pro.local>
2024-09-03 15:18:12 -0500

    Models

--- api/models.py
+++ api/models.py
@@ -24,6 +24,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     reset_token = models.CharField(max_length=100, blank=True, null=True)
 
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
